85d8c064b1cc97a0d24f08964d2ff6bcba30fec5
rbnpappu <thakurpappu905@gmail.com>
2025-06-27 12:01:23 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: [...allowedOrigins],
+  origin: 'https://mini-blog-frontend-dvnj.vercel.app',
   optionsSuccessStatus: 200,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
