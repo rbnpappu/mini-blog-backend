909e3a8094f0b0c96f48a5b3c57103ea522eff30
rbnpappu <thakurpappu905@gmail.com>
2025-06-27 11:55:11 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [...allowedOrigins],
+  optionsSuccessStatus: 200,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
